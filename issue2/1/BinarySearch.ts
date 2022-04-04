acdc0a2291c4c6d9f3478e5f1163f35cf0ee38a7
Pavel Polyakov <pavpoly.tch@gmail.com>
2022-04-04 12:11:22 +0300

    feat(issue2): added descriptions, wrote tests for 2nd task, changed README
    added descriptions, wrote tests for 2nd task, changed README

--- issue2/1/BinarySearch.ts
+++ issue2/1/BinarySearch.ts
@@ -1,4 +1,20 @@
+// Binary Search Algorithm:
+
+// Begin with an interval covering the whole array.
+// If the value of the search key is less than the item in the middle of the interval, 
+//    narrow the interval to the lower half.
+// Otherwise, narrow it to the upper half.
+// Repeatedly check until the value is found or the interval is empty.
+
 export default class BinarySearch {
+  /**
+   *
+   * @param {number} needle - An element, which we search in given array(haystack)
+   * @param {array} haystack - An assorted array of natural numbers
+   * @returns {string} - Result of search as a string, where
+   *          "Да" means presence of needle in haystack and
+   *          "Нет" means it's abscence
+   */
   public static search(needle: number, haystack: Array<number>): string {
     const sortedHaystack = haystack.sort(function (a: number, b: number) {
       return a - b;
---
+++ issue2/2/__tests__/sort.test.ts
@@ -0,0 +1,32 @@
+describe("sorting algorithms", () => {
+  const assorted: Array<number> = [-3, -1, 2, 4, -5, 0, 7, -2];
+  const sorted: Array<number> = [-5, -3, -2, -1, 0, 2, 4, 7];
+
+  it("selection sort works", () => {
+    expect(SelectionSort.sort(assorted)).toEqual([sorted]);
+  });
+
+  it("bubble sort works", () => {
+    expect(BubbleSort.sort(assorted)).toEqual([sorted]);
+  });
+
+  it("insertion sort works", () => {
+    expect(InsertionSort.sort(assorted)).toEqual([sorted]);
+  });
+
+  it("merge sort works", () => {
+    expect(MergeSort.sort(assorted)).toEqual([sorted]);
+  });
+
+  it("heap sort works", () => {
+    expect(HeapSort.sort(assorted)).toEqual([sorted]);
+  });
+
+  it("quick sort works", () => {
+    expect(QuickSort.sort(assorted)).toEqual([sorted]);
+  });
+  
+  it("radix sort works", () => {
+    expect(RadixSort.sort(assorted)).toEqual([sorted]);
+  });
+});

fe7d72211da7526075fc022f2752ea1e12e4f451
Pavel Polyakov <pavpoly.tch@gmail.com>
2022-04-05 19:56:20 +0300

    feat(issue2): changed test, wrote CountingSort, passed tests
    changed test, wrote CountingSort, passed tests

---
+++ issue2/2/CountingSort.ts
@@ -0,0 +1,61 @@
+// Counting sort is a sorting algorithm that sorts the elements of an array by
+//      counting the number of occurrences of each unique element in the array.
+// The count is stored in an auxiliary array and the sorting is done by
+//      mapping the count as an index of the auxiliary array.
+
+export default class CountingSort {
+  /**
+   *
+   * @param {array} assortedInp - An assorted array of natural numbers
+   * @returns {array} - Result of sorting the assorted array
+   */
+  public static sort(assortedInp: Array<number>): Array<number> {
+    // Clone array to avoid side effects to original one
+    let assorted = [...assortedInp];
+
+    // Check if no negative numbers
+    if (
+      !assorted.every((num) => {
+        return num >= 0;
+      })
+    ) {
+      throw new Error("error(negative numbers in array)");
+    }
+
+    // Find max number
+    let max: number = assorted[0];
+    assorted.forEach((elem) => {
+      if (elem > max) {
+        max = elem;
+      }
+    });
+
+    // Count array and final array
+    let aux = new Array(max + 1);
+    let sorted: Array<number> = new Array(assorted.length);
+
+    // Init count array
+    for (let i = 0; i < aux.length; i++) {
+      aux[i] = 0;
+    }
+
+    // Store number of occurrences of each element
+    for (let i = 0; i < assorted.length; i++) {
+      aux[assorted[i]] += 1;
+    }
+
+    // Turn count array into cumulative sum of elements
+    for (let i = 1; i < aux.length; i++) {
+      aux[i] += aux[i - 1];
+    }
+
+    // Place items according to count array into new one
+    for (let i = 0; i < assorted.length; i++) {
+      const element = assorted[i];
+      sorted[aux[element] - 1] = element;
+      aux[element] -= 1;
+    }
+
+    return sorted;
+  }
+}
--- issue2/2/__tests__/Sorts.test.ts
+++ issue2/2/__tests__/Sorts.test.ts
@@ -4,6 +4,7 @@ import InsertionSort from "../InsertionSort";
 import MergeSort from "../MergeSort";
 import HeapSort from "../HeapSort";
 import QuickSort from "../QuickSort";
+import CountingSort from "../CountingSort";
 
 describe("sorting algorithms", () => {
   const assorted: Array<number> = [-3, -1, 2, 4, -5, 0, 7, -2];
@@ -32,8 +33,16 @@ describe("sorting algorithms", () => {
   it("quick sort works", () => {
     expect(QuickSort.sort(assorted)).toEqual(sorted);
   });
-  
-  it("radix sort works", () => {
-    expect(RadixSort.sort(assorted)).toEqual(sorted);
+
+  it("counting sort works", () => {
+    const assorted: Array<number> = [38, 27, 43, 3, 9, 82, 10];
+    const sorted: Array<number> = [3, 9, 10, 27, 38, 43, 82];
+    expect(CountingSort.sort(assorted)).toEqual(sorted);
+  });
+
+  it("counting sort doesn't work on negative numbers", () => {
+    expect(() => {
+      CountingSort.sort(assorted);
+    }).toThrowError("error(negative numbers in array)");
   });
 });

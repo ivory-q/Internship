5471854065ff4e34dd8e0a787949e80d96db4324
Pavel Polyakov <pavpoly.tch@gmail.com>
2022-04-05 21:55:04 +0300

    refactor(issue2): modified test to test array capablity of BinarySearch

--- issue2/1/__tests__/BinarySearch.test.ts
+++ issue2/1/__tests__/BinarySearch.test.ts
@@ -25,34 +25,16 @@ describe("binary search", () => {
   describe("given example tests", () => {
     const haystack: Array<number> = [-3, -2, 0, 1, 4, 6, 9, 12, 15, 18, 22];
 
-    it("-2 Да", () => {
-      const needle = -2;
+    it("passes test from example", () => {
+      const needle = [-2, 3, 5, 12, 22];
 
-      expect(BinarySearch.search([needle], haystack)).toEqual(["-2 Да"]);
-    });
-
-    it("3 Нет", () => {
-      const needle = 3;
-
-      expect(BinarySearch.search([needle], haystack)).toEqual(["3 Нет"]);
-    });
-
-    it("5 Нет", () => {
-      const needle = 5;
-
-      expect(BinarySearch.search([needle], haystack)).toEqual(["5 Нет"]);
-    });
-
-    it("12 Да", () => {
-      const needle = 12;
-
-      expect(BinarySearch.search([needle], haystack)).toEqual(["12 Да"]);
-    });
-
-    it("22 Да", () => {
-      const needle = 22;
-
-      expect(BinarySearch.search([needle], haystack)).toEqual(["22 Да"]);
+      expect(BinarySearch.search(needle, haystack)).toEqual([
+        "-2 Да",
+        "3 Нет",
+        "5 Нет",
+        "12 Да",
+        "22 Да",
+      ]);
     });
   });
 });
